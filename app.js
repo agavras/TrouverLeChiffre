a0cc60dfa0054cf0ffa58c1ec2e8098ded1cfb40
agavras <math.falze@gmail.com>
2022-03-29 19:25:05 +0200

    Retour du champ de texte lié au slider + mises en pages...

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const btn = document.getElementById("HTMLidBoutton");
 const chiffreATrouver = Math.ceil(Math.random() * 100);
 console.log(chiffreATrouver);
-// const elementChampDeTextePourLaReponseDuJoueur = document.getElementById("HTMLid_InputNumber");
+const elementChampDeTextePourLaReponseDuJoueur = document.getElementById("HTMLid_InputNumber");
 const mySlider=document.getElementById("myRange");
-const myValueText=document.getElementById("valueText");
 const elementTexteIlVousReste = document.getElementById("HTMLid_Texte_IlVousReste");
 const elementTexteWinLoose = document.getElementById("HTMLid_Texte_YouWinOrLoose");
 
@@ -12,8 +11,16 @@ const elementTexteWinLoose = document.getElementById("HTMLid_Texte_YouWinOrLoose
 let tentativeRestante = 10;
 let MyAudio = new Audio("sound/music1.mp3");
 
+elementTexteIlVousReste.innerHTML = `il vous reste ${tentativeRestante} tentative(s)`;
+
+// mise a jour Slider => InputNumber...
 mySlider.oninput = function() {
-myValueText.innerHTML = parseInt(mySlider.value);
+    elementChampDeTextePourLaReponseDuJoueur.value = mySlider.value;
+}
+
+// mise a jour InputNumber => Slider...
+elementChampDeTextePourLaReponseDuJoueur.oninput = function() {
+    mySlider.value = elementChampDeTextePourLaReponseDuJoueur.value;
 }
 
 // Fonction quand on appui sur le btn...
@@ -21,14 +28,18 @@ function CheckTheResult() {
     MyAudio.play();
     let chiffreDuJoueur=parseInt(mySlider.value);
     tentativeRestante--;
-    elementTexteIlVousReste.innerHTML = `il vous reste ${tentativeRestante} tentative(s)`;
+    if (tentativeRestante>1) {
+        elementTexteIlVousReste.innerHTML = `il vous reste ${tentativeRestante} tentatives`;
+    } else {
+        elementTexteIlVousReste.innerHTML = `il vous reste ${tentativeRestante} tentative`;
+    }
 
     // Si le resultat est bon...
     if (chiffreATrouver === chiffreDuJoueur) {
         elementTexteWinLoose.style.color="lightgreen";
         elementTexteWinLoose.innerHTML = "YOU WIN";
         elementTexteWinLoose.className="animate__animated animate__bounce animate__infinite";
-        mySlider.className="animate__animated animate__zoomOutDown animate__1";
+        mySlider.remove();
         MyAudio.pause();
         MyAudio = new Audio("sound/musicYouWin.mp3");
         MyAudio.play();
@@ -47,20 +58,13 @@ function CheckTheResult() {
     if (tentativeRestante === 0) {
         elementTexteWinLoose.style.color="red";
         elementTexteWinLoose.innerHTML = "GAME OVER";
-        // DeleteIlVousReste();
         elementTexteWinLoose.className="animate__animated animate__bounceInUp animate__1";
-        mySlider.className="animate__animated animate__hinge animate__1";
+        mySlider.remove();
         MyAudio.pause();    
         MyAudio = new Audio("sound/musicGameOver.mp3");
         MyAudio.play();
         elementTexteIlVousReste.className="animate__animated animate__bounceOutDown animate__1";
         btn.className="animate__animated animate__bounceOut animate__1";
-        myValueText.className="animate__animated animate__bounceOut animate__1";
+        elementChampDeTextePourLaReponseDuJoueur.className="animate__animated animate__hinge animate__1";
     }
 } // fin de la function CheckTheResult()
-
-// Fonction qui remplace le texte "Il Vous reste" par un espace et detruit le btn...
-function DeleteIlVousReste() {
-    elementTexteIlVousReste.innerHTML = " "; // on peut aussi utiliser .remove();
-    btn.remove();
-}// fin de la function DeleteIlVousReste()
